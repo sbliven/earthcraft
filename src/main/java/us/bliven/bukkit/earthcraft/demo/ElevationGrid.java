6b3f4fbf5ff14b513d4f1a7c04d6ea7e1620d171
Spencer Bliven <sbliven@ucsd.edu>
2013-09-05 18:22:27 -0700

    Adding white 10px scale bar to ElevationGrid

--- src/main/java/us/bliven/bukkit/earthcraft/demo/ElevationGrid.java
+++ src/main/java/us/bliven/bukkit/earthcraft/demo/ElevationGrid.java
@@ -38,26 +38,26 @@ public class ElevationGrid {
 		double north = 32.75;
 		double east = -117.15;
 		Unit<Length> degLat = NonSI.NAUTICAL_MILE.times(60); //Nautical_mile = 1 arcminute
-		double latRes = Measure.valueOf(10.0, SI.METER).doubleValue(degLat);
+		double latRes = Measure.valueOf(30.0, SI.METER).doubleValue(degLat);
 		double lonRes = latRes;
 		System.out.format("Area %f deg^2 at %f res = %d points%n",
 				(north-south)*(east-west), latRes,
 				((int)((north-south)/latRes))*((int)((east-west)/lonRes)));
 		//double latRes = (north-south)/60.;
 		//double lonRes = (east-west)/100.;
-		
+
 		//OpenElevationConnector oec = new OpenElevationConnector();
 		//elevationProvider = oec;
 		//elevationProvider = new ElevationProviderStub(south+10*latRes,north-10*latRes, west+10*lonRes,east-10*lonRes,0.,255.);
-		
+
 		String dir = "/Users/blivens/dev/minecraft/srtm";
 		if(! (new File(dir)).exists()) {
 			dir = System.getProperty("java.io.tmpdir") + "SRTMPlus";
 		}
 		elevationProvider = new SRTMPlusElevationProvider(dir);
-		
+
 		elevationCache = new InterpolatingElevationCache(elevationProvider, new Coordinate(latRes,lonRes));
-		
+
 		Double[][] elevations = elevationGrid(elevationCache, south,north,west,east, latRes/2, lonRes/2);
 
 		/*
@@ -66,7 +66,7 @@ public class ElevationGrid {
 			for(int j=0;j<ypoints;j++)
 				elevations[i][j] = (double)((3*i+2*j)%256);
 		*/
-		
+
 		BufferedImage image = elevationImage(elevations);
 
 		displayImage(image);
@@ -102,12 +102,12 @@ public class ElevationGrid {
 
 	/**
 	 * Creates a heatmap for a matix of doubles.
-	 * 
-	 * The input should be in [y][x] order and have the origin at the bottom 
+	 *
+	 * The input should be in [y][x] order and have the origin at the bottom
 	 * left of the resulting image. Thus [0][0] is the bottom left pixel,
 	 * [0][n] is the bottom right, [m][0] the top left and [m][n] is the top right.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param elevations A matrix of doubles
 	 * @return A BufferedImage containing one pixel per input element
 	 */
@@ -123,7 +123,7 @@ public class ElevationGrid {
 				// Nulls are black
 				if(elevations[y][x] == null || elevations[y][x]<0) {
 					pixels[i] = pixels[i+1] = pixels[i+2] = 0;
-					
+
 				}
 				else {
 					//pixels[i] = pixels[i+1] = pixels[i+2] = elevations[x][y].intValue();
@@ -133,7 +133,7 @@ public class ElevationGrid {
 						pixels[i] = 0;
 						pixels[i+1] = 0;
 						pixels[i+2] = 255;
-					} else { 
+					} else {
 						// Interpolate between green (0x00ff00)
 						// and brown 0x804000
 						int v = elevations[y][x].intValue();
@@ -142,6 +142,15 @@ public class ElevationGrid {
 						pixels[i+1] = 255-3*v/4;
 						pixels[i+2] = 0;
 					}
+				}
+
+				// 10px white scale bar at top left
+				if( 2 <= x && x<= 12 && y == 2 ||
+						(x==2 || x==12) && y==3)
+				{
+					pixels[i] = 255;
+					pixels[i+1] = 255;
+					pixels[i+2] = 255;
 				}
 			}
 		}
@@ -158,7 +167,7 @@ public class ElevationGrid {
 				new int[] {8,8,8}, false, false,
 				Transparency.OPAQUE,
 				DataBuffer.TYPE_INT);
-		
+
 		SampleModel sampleModel = colorModel.createCompatibleSampleModel(width, height);
 
 		// Create a raster using the sample model and data buffer
@@ -168,7 +177,7 @@ public class ElevationGrid {
 		// Combine the color model and raster into a buffered image
 		BufferedImage image = new BufferedImage(colorModel, raster, false, null);//new java.util.Hashtable());
 		*/
-		
+
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		WritableRaster raster = image.getRaster();
 		raster.setPixels(0, 0, width, height, pixels);
@@ -176,14 +185,14 @@ public class ElevationGrid {
 	}
 
 	/**
-	 * Fetches the elevation for a rectangular chunk of the earth, sampling 
+	 * Fetches the elevation for a rectangular chunk of the earth, sampling
 	 * at regular intervals
 	 * @param p	The ElevationProvider to supply elevations
 	 * @param south South border of the rectangle to sample
 	 * @param north North border of the rectangle to sample
 	 * @param east East border of the rectangle to sample
 	 * @param west West border of the rectangle to sample
-	 * @param latRes North-south sampling frequency 
+	 * @param latRes North-south sampling frequency
 	 * @param lonRes East-west sampling frequency
 	 * @return A matrix of elevations indexed [lat][lon], starting with [0][0]
 	 *  at the south-western corner, moving eastward through [0][x], and finally
@@ -197,9 +206,9 @@ public class ElevationGrid {
 		if(latRes<=0 || lonRes<=0) throw new IllegalArgumentException("Too high resolution");
 		int xpoints = (int)Math.ceil((east-west)/lonRes);
 		int ypoints = (int)Math.ceil((north-south)/latRes);
-		
+
 		if(xpoints<=0 || ypoints <=0) throw new IllegalArgumentException("Empty Region");
-		
+
 		Double[][] elevation = new Double[ypoints][];
 		for(int y=0;y<ypoints;y++) {
 			// Same y coordinate for this whole row of elevations
@@ -211,7 +220,7 @@ public class ElevationGrid {
 				double xcoord = west + x*lonRes;
 				points.add(new Coordinate(ycoord,xcoord));
 			}
-			
+
 			// Fetch row of elevations
 			List<Double> elevations = null;
 			try {
