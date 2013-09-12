e037490836beddd31adc5fda5db2b1dc2bb5838f
Spencer Bliven <sbliven@ucsd.edu>
2013-09-11 17:39:36 -0700

    Adding interpolation member variable.
    It's hard-coded in the constructor, but might be configurable later.

--- src/main/java/us/bliven/bukkit/earthcraft/gis/InterpolatedCoverageElevationProvider.java
+++ src/main/java/us/bliven/bukkit/earthcraft/gis/InterpolatedCoverageElevationProvider.java
@@ -2,6 +2,9 @@ package us.bliven.bukkit.earthcraft.gis;
 
 import java.util.concurrent.Callable;
 
+import javax.media.jai.Interpolation;
+import javax.media.jai.InterpolationBicubic;
+
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.Interpolator2D;
 import org.opengis.coverage.PointOutsideCoverageException;
@@ -11,11 +14,17 @@ import com.vividsolutions.jts.geom.Coordinate;
 public class InterpolatedCoverageElevationProvider extends
 		GridCoverageElevationProvider {
 
-	GridCoverageElevationProvider provider;
+	private final GridCoverageElevationProvider provider;
+	private final Interpolation interpolation;
 
 
 	public InterpolatedCoverageElevationProvider(GridCoverageElevationProvider provider) {
 		this.provider = provider;
+
+		interpolation = new InterpolationBicubic(16);
+		//interpolation = new InterpolationBilinear();
+		//interpolation = new InterpolationNearest();
+
 	}
 
 	@Override
@@ -27,7 +36,7 @@ public class InterpolatedCoverageElevationProvider extends
 	protected Callable<GridCoverage2D> createTileLoader(Coordinate coord) {
 		Callable<GridCoverage2D> provider = this.provider.createTileLoader(coord);
 
-		return new InterpolatedCoverageLoader(provider);
+		return new InterpolatedCoverageLoader(provider,interpolation);
 	}
 
 	@Override
@@ -44,17 +53,20 @@ public class InterpolatedCoverageElevationProvider extends
 
 	protected static class InterpolatedCoverageLoader implements Callable<GridCoverage2D> {
 		private Callable<GridCoverage2D> provider;
+		private Interpolation interpolation;
 
-		public InterpolatedCoverageLoader(Callable<GridCoverage2D> provider) {
+		public InterpolatedCoverageLoader(Callable<GridCoverage2D> provider, Interpolation interpolation) {
 			this.provider = provider;
+			this.interpolation = interpolation;
 		}
 
 		@Override
 		public GridCoverage2D call() throws Exception {
 			GridCoverage2D coverage = provider.call();
 
-			GridCoverage2D interpolated = Interpolator2D.create(coverage);
 
+			GridCoverage2D interpolated;
+			interpolated = Interpolator2D.create(coverage,this.interpolation);
 			return interpolated;
 		}
 
